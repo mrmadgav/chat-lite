d8d601998c62918f5c7ef4d9827d31fb8119cfed
madgav <madgav@mail.ru>
2021-08-06 23:13:33 +0600

    install react-router-dom

--- src/components/helpers/io.js
+++ src/components/helpers/io.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://chat-lite-back.herokuapp.com/", {
+export const socket = io("https://chat-lite-back.herokuapp.com", {
   withCredentials: true
 });
