2801e2d55f9f686ef70c9bd5b3433f65d9767caf
madgav <madgav@mail.ru>
2021-08-08 01:31:14 +0600

    URL Versel

--- src/components/helpers/io.js
+++ src/components/helpers/io.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://chat-lite-back.herokuapp.com/", {
+export const socket = io("https://chat-lite-two.vercel.app/", {
   withCredentials: true,
 });
 // https://chat-lite-back.herokuapp.com/
