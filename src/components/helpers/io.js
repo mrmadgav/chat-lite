985f6f249319d27f0dffc4e3f9cac704ac7c741d
madgav <madgav@mail.ru>
2021-08-08 14:22:20 +0600

    empty io

--- src/components/helpers/io.js
+++ src/components/helpers/io.js
@@ -1,9 +1,10 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://chat-lite-back.herokuapp.com/", {
-  withCredentials: true,
-  extraHeaders: {
-    "my-custom-header": "abcd",
-  },
-});
+export const socket = io();
 // https://chat-lite-back.herokuapp.com/
+// "https://chat-lite-back.herokuapp.com/", {
+//   withCredentials: true,
+//   extraHeaders: {
+//     "my-custom-header": "abcd",
+//   },
+// }
