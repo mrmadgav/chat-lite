0a44b148abceaed9dc610a861786f333390fcd71
madgav <madgav@mail.ru>
2021-08-08 14:50:48 +0600

    remove listeners

--- src/components/MessageFlow/MessageFlow.jsx
+++ src/components/MessageFlow/MessageFlow.jsx
@@ -78,6 +78,7 @@ export default function MessageFlow(props) {
       getMessageFromServer();
       dispatch(fetchHistory());
       scrollToBottom();
+      socket.removeListener("message:fromServer");
     });
 
     socket.on("userTyping", userTyping);
