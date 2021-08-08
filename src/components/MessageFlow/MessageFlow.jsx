88ca1bc1a9a2b0be4ce930a1aaeb12fe3694e288
madgav <madgav@mail.ru>
2021-08-08 15:02:39 +0600

    fixing listeners

--- src/components/MessageFlow/MessageFlow.jsx
+++ src/components/MessageFlow/MessageFlow.jsx
@@ -51,7 +51,7 @@ export default function MessageFlow(props) {
     console.log("КТО-ТО НАПИСАЛ СООБЩЕНИЕ");
     dispatch(fetchHistory()).then(() => scrollToBottom());
 
-    socket.off("message:fromServer");
+    socket.removeListener("message:fromServer");
   });
 
   socket.on("message:edited", () => {
