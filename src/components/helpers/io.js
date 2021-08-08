2d458d85b9989e3d24600ff0387c4c67d17e926b
madgav <madgav@mail.ru>
2021-08-08 13:16:19 +0600

    add headers from manual socket io

--- src/components/helpers/io.js
+++ src/components/helpers/io.js
@@ -1,4 +1,9 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://chat-lite-back.herokuapp.com/");
+export const socket = io("https://chat-lite-back.herokuapp.com/", {
+  withCredentials: true,
+  extraHeaders: {
+    "my-custom-header": "abcd",
+  },
+});
 // https://chat-lite-back.herokuapp.com/
