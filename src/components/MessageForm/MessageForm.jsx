fc6339a51e50c3477bb0085f702bd41a5f5aeebe
madgav <madgav@mail.ru>
2021-08-14 17:48:30 +0600

    fixing pnotify multiple evts

--- src/components/MessageForm/MessageForm.jsx
+++ src/components/MessageForm/MessageForm.jsx
@@ -68,10 +68,16 @@ export default function MessageForm(props) {
       setMessage("");
       setPicker(false);
     } else {
+      // notificationManager.cancelAll();
       notice({
         text: "Агрессивное молчание не принесет тебе удовлетворения",
         styling: "brighttheme",
         delay: 1000,
+        text_escape: true,
+        nonblock: {
+          nonblock: true,
+          nonblock_opacity: 0.1,
+        },
       });
     }
   };
