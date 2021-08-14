82f638e9fd9986550c33474932c0c9f552a9cc8c
madgav <madgav@mail.ru>
2021-08-14 18:21:27 +0600

    still fighting with pnotify

--- src/components/MessageForm/MessageForm.jsx
+++ src/components/MessageForm/MessageForm.jsx
@@ -68,13 +68,13 @@ export default function MessageForm(props) {
       setMessage("");
       setPicker(false);
     } else {
-      notice?.close();
-      notice({
-        text: "Агрессивное молчание не принесет тебе удовлетворения",
-        styling: "brighttheme",
-        delay: 1000,
-        killer: true,
-      });
+      !notice &&
+        notice({
+          text: "Агрессивное молчание не принесет тебе удовлетворения",
+          styling: "brighttheme",
+          delay: 1000,
+          killer: true,
+        });
     }
   };
 
