468e0a9130dde4d8754fe233904d4661468592c3
madgav <madgav@mail.ru>
2021-08-15 01:46:37 +0600

    modal is done

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,18 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import styles from "./Modal.module.css";
 
 export default function Modal(props) {
   function handlekeydown(e) {
     if (e.code === "Escape") {
-      console.log("вызван handlekeydown");
-      console.log(props);
       props.toggleModal();
     }
   }
   const largeImgUrl = props.imgUrl.replace("c_fill,w_300/", "");
 
   useEffect(() => {
-    console.log("Маунт Модалки");
     window.addEventListener("keydown", handlekeydown);
     return () => {
       window.removeEventListener("keydown", handlekeydown);
