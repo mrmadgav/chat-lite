54b9c49c2cd9067a9f986c6547c2377120407645
madgav <madgav@mail.ru>
2021-08-15 00:31:28 +0600

    change modal to RFC

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,25 +1,32 @@
 import React, { Component } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import styles from "./Modal.module.css";
 
-export default class Modal extends Component {
-  componentDidMount() {
+export default function Modal(props) {
+  const [Modal, setModal] = useState(false);
+
+  useEffect(() => {
     window.addEventListener("keydown", this.handlekeydown);
+    return () => {
+      window.removeEventListener("keydown", this.handlekeydown);
+    };
+  }, []);
+  function toggleModal() {
+    setModal(!Modal);
   }
-  componentWillUnmount() {
-    window.removeEventListener("keydown", this.handlekeydown);
-  }
-  handlekeydown = (e) => {
+
+  function handlekeydown(e) {
     if (e.code === "Escape") {
-      this.props.toggleModal();
+      toggleModal();
     }
-  };
-  render() {
-    return (
-      <div className={styles.Overlay} onClick={this.props.toggleModal}>
-        <div className={styles.Modal}>
-          <img src={this.props.currentImg.largeImageURL} alt="" />
-        </div>
-      </div>
-    );
   }
+
+  return (
+    <div className={styles.Overlay} onClick={toggleModal}>
+      <div className={styles.Modal}>
+        <img src={props.imgUrl} alt="" />
+      </div>
+    </div>
+  );
 }
