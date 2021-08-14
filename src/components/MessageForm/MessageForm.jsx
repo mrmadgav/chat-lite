a442b51586def055e577ddbe418d6a559deeeac7
madgav <madgav@mail.ru>
2021-08-14 18:06:56 +0600

    fixing pnotify

--- src/components/MessageForm/MessageForm.jsx
+++ src/components/MessageForm/MessageForm.jsx
@@ -68,8 +68,7 @@ export default function MessageForm(props) {
       setMessage("");
       setPicker(false);
     } else {
-      // notice?.removeAll();
-      notice?.remove();
+      notice?.close();
       notice({
         text: "Агрессивное молчание не принесет тебе удовлетворения",
         styling: "brighttheme",
