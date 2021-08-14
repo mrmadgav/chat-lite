9f004b12c7a90bb5e00d9f4f9108b927e754eb8d
madgav <madgav@mail.ru>
2021-08-14 15:40:31 +0600

    add font for logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import styles from "./Logo.module.css"
 
 export default function Logo() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <span className={styles.Logo}>Le Chat</span>
+    </div>
+  );
 }
