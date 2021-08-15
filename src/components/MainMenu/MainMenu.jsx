f99915dc0166676e88290f06d554718be8ea7a80
madgav <madgav@mail.ru>
2021-08-15 09:58:00 +0600

    add logo to home page

--- src/components/MainMenu/MainMenu.jsx
+++ src/components/MainMenu/MainMenu.jsx
@@ -1,32 +1,36 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from "./MainMenu.module.css";
+import Logo from "../Logo/Logo";
 
 const MainMenu = () => {
   return (
-    <div className={styles.MenuContainer}>
-      <ul className={styles.MainBar}>
-        <li className={styles.navLi}>
-          <NavLink
-            exact
-            to="/register"
-            className={styles.NavLink}
-            activeClassName={styles.NavLinkActive}
-          >
-            Register
-          </NavLink>
-        </li>
-        <li className={styles.navLink}>
-          <NavLink
-            exact
-            to="/login"
-            className={styles.NavLink}
-            activeClassName={styles.NavLinkActive}
-          >
-            Login
-          </NavLink>
-        </li>
-      </ul>
+    <div>
+      <Logo />
+      <div className={styles.MenuContainer}>
+        <ul className={styles.MainBar}>
+          <li className={styles.navLi}>
+            <NavLink
+              exact
+              to="/register"
+              className={styles.NavLink}
+              activeClassName={styles.NavLinkActive}
+            >
+              Register
+            </NavLink>
+          </li>
+          <li className={styles.navLink}>
+            <NavLink
+              exact
+              to="/login"
+              className={styles.NavLink}
+              activeClassName={styles.NavLinkActive}
+            >
+              Login
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
