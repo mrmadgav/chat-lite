c590ade2006789d30ec588dfe11ea3c144eb9c04
madgav <madgav@mail.ru>
2021-08-17 21:10:48 +0600

    fix  deleting private func

--- src/components/MessageMenu/MessageMenu.jsx
+++ src/components/MessageMenu/MessageMenu.jsx
@@ -75,7 +75,7 @@ export default function CustomizedMenus(props) {
         <ListItemText
           primary="Delete"
           onClick={() => {
-            dispatch(onDelete({ id: id }, roomId));
+            dispatch(onDelete({ id: id, roomId: roomId }));
             // socket.emit("message:delete", id);
             props.handleToUpdate(id);
             setClose(true);
