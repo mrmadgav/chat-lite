b4d206863cc7ec40127f5ceda7a4c628b43823c2
madgav <madgav@mail.ru>
2021-08-18 11:02:02 +0600

    fixing add active class

--- src/components/ChatList/ChatList.jsx
+++ src/components/ChatList/ChatList.jsx
@@ -39,10 +39,14 @@ export default function ChatList() {
   }, [allUsers]);
 
   const addActiveClass = (e) => {
-    if (e.target.tagName === "SPAN")
-      for (let i = 0; i < e.currentTarget.childNodes.length; i++) {
-        e.currentTarget.childNodes[i].classList.remove("active");
-      }
+    if (e.target.tagName === "SPAN") console.log("this");
+    console.log(
+      "e.currentTarget.childNodes.length",
+      e.currentTarget.childNodes.length
+    );
+    for (let i = 0; i < e.currentTarget.childNodes.length; i++) {
+      e.currentTarget.childNodes[i].classList.remove("active");
+    }
     e.target.classList.add("active");
   };
 
