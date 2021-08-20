079c3a1c3ae19d8c3d31c6a3b4184adadcbaa77f
madgav <madgav@mail.ru>
2021-08-21 00:54:02 +0600

    add private evts on front to another useEffect

--- src/components/MessageFlow/MessageFlow.jsx
+++ src/components/MessageFlow/MessageFlow.jsx
@@ -113,6 +113,7 @@ function MessageFlow(props) {
 
     return () => {
       console.log("Анмаунт useEffect со слушателями привата");
+      console.log("Анмаунт useEffect со слушателями привата");
       socket.removeListener("privateMessage:fromServer");
       socket.removeListener("privateDeleteMessage:fromServer");
       socket.removeListener("privateEditMessage:fromServer");
