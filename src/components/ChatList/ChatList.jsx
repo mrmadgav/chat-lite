f4cbbfcd5c637fd023988355ccd33c01127d1dbc
madgav <madgav@mail.ru>
2021-08-20 12:53:18 +0600

    working on direct messages counter

--- src/components/ChatList/ChatList.jsx
+++ src/components/ChatList/ChatList.jsx
@@ -1,44 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllUsers, getRoomId, getUser } from "../../Redux/selectors";
+import { getAllUsers, getUser } from "../../Redux/selectors";
 
 import styles from "./ChatList.module.css";
 import { setRoomId } from "../../Redux/Chat/Chat-operations";
 import CounterDirectMessages from "../CounterDirectMessages/CounterDirectMessages";
-import { socket } from "../helpers/io";
 
-export default function ChatList() {
-  const [counter, setCounter] = useState(0);
-
+export default function ChatList() { 
   const getUserId = useSelector(getUser);
   const allUsers = useSelector(getAllUsers);
   const dispatch = useDispatch();
-  const currentRoomId = useSelector(getRoomId);
-
-  // useEffect(() => {
-  //   socket.on("privateMessage:fromServer", (id) => {
-  //     (id !== currentRoomId && id !== reverseRoomId(currentRoomId)) && setCounter(counter++)}
-  //   return () => {}
-  // }, [])
-
-  function reverseRoomId(roomId) {
-    const firstPart = roomId.substr(0, roomId.length / 2);
-    const secondPart = roomId.substr(roomId.length / 2);
-    let newStr = [secondPart, firstPart].join("");
-    return newStr;
-  }
-
-  useEffect(() => {
-    console.log("сработал UseEffect");
-    socket.on("privateMessage:fromServer", (id) => {
-      id !== currentRoomId &&
-        id !== reverseRoomId(currentRoomId) &&
-        setCounter(counter + 1);
-    });
-    return () => {
-      socket.removeListener("privateMessage:fromServer");
-    };
-  }, [counter, currentRoomId]);
 
   // Функционал личных сообщений
   //начать диалог (создать комнату)
@@ -90,7 +61,7 @@ export default function ChatList() {
                     onClick={beginPrivateDialog}
                   >
                     {i.nickname}
-                    <CounterDirectMessages counter={counter} />
+                    <CounterDirectMessages id={i._id}/>
                   </span>
                 </li>
               );
--- src/components/CounterDirectMessages/CounterDirectMessages.jsx
+++ src/components/CounterDirectMessages/CounterDirectMessages.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useSelector, useEffect, useState } from "react";
+import { getRoomId } from "../../Redux/selectors";
 import styles from "./CounterDirectMessages.module.css";
+import { socket } from "../helpers/io";
 
 export default function CounterDirectMessages(props) {
+  const currentRoomId = useSelector(getRoomId);
+  const [counter, setCounter] = useState(0);
+
+  function reverseRoomId(roomId) {
+    const firstPart = roomId.substr(0, roomId.length / 2);
+    const secondPart = roomId.substr(roomId.length / 2);
+    let newStr = [secondPart, firstPart].join("");
+    return newStr;
+  }
+
+  useEffect(() => {
+    console.log("сработал UseEffect");
+    socket.on("privateMessage:fromServer", (id) => {
+      id !== (currentRoomId && reverseRoomId(currentRoomId)) &&
+        (id = props.id) &&
+        setCounter(counter + 1);
+    });
+    return () => {
+      socket.removeListener("privateMessage:fromServer");
+    };
+  }, [counter, currentRoomId]);
+
   return (
-    <span className={`${styles.counter} ${styles.hidden} `}>
-      {props.counter}
-    </span>
+    <span className={`${styles.counter} ${styles.hidden} `}>{counter}</span>
   );
 }
