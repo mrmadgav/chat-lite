c58545b9b664d0ecb0198ec77e92a960adbcd4de
madgav <madgav@mail.ru>
2021-08-21 19:17:15 +0600

    add active chat to localstorage

--- src/components/MainMenu/MainMenu.jsx
+++ src/components/MainMenu/MainMenu.jsx
@@ -30,7 +30,7 @@ const MainMenu = () => {
                 height="30px"
                 margin-right="10"
               />
-              Register
+              <span className={styles.navigationSpan}>Register</span>
             </NavLink>
           </li>
           <li className={styles.navLink}>
