d6200e0be1f077bbaaacc547a13d4ce9dce87a40
madgav <madgav@mail.ru>
2021-08-21 21:50:48 +0600

    fixing errors evts in Redux

--- src/Redux/Auth/Auth-operations.js
+++ src/Redux/Auth/Auth-operations.js
@@ -21,20 +21,20 @@ export const register = (credentials) => async (dispatch) => {
     dispatch(authActions.registerSuccess(response.data));
     dispatch(login(credentials));
   } catch (error) {
-    console.log(error);
     dispatch(authActions.registerError(error.message));
   }
 };
 
-export const login = (credentials) => async (dispatch) => {
+export const login = (credentials, res) => async (dispatch) => {
   dispatch(authActions.LoginRequest());
   try {
     const response = await axios.post("/login", credentials);
     token.set(response.data.data.token);
     dispatch(authActions.LoginSuccess(response.data));
   } catch (error) {
+    console.log(res);
     dispatch(authActions.LoginError(error.message));
-    dispatch(authActions.getUsersError(error.message));
+    // dispatch(authActions.getUsersError(error.message));
   }
 };
 
